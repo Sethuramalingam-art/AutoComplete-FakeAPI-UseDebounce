460456ba00ce35d4b7a67f1479c1fab1fc729b63
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2025-12-17 12:52:43 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import './App.css';
+import React, { useState } from 'react'
+import AutoComplete from './AutoComplete';
+
+function App() {
+  return <AutoComplete></AutoComplete>
+}
+
+export default App;
